c63caddf4a5236e7b6058e21c25eeda1104ae314
Emmanuel <laryeaaristotle@gmail.com>
2025-06-25 22:51:57 +0000

    frontend for admin complete

--- app/admins/views.py
+++ app/admins/views.py
@@ -29,3 +29,48 @@ def users_management_view(request):
 # roles and permissions
 def roles_permissions(request):
     return render(request, 'roles.html', {})
+
+
+# appointments
+def appointments(request):
+    return render(request, 'appointments.html', {})
+
+
+# manage schedules
+def manage_schedules(request):
+    return render(request, 'manage_schedules.html', {})
+
+
+# patient records
+def patient_record(request):
+    return render(request, 'patient_record.html', {})
+
+
+# appointment report
+def appointment_report(request):
+    return render(request, 'appointment_report.html', {})
+
+
+# patient activities
+def patient_activities(request):
+    return render(request, 'patient_activities.html', {})
+
+
+# audit logs
+def audit_logs(request):
+    return render(request, 'audit_logs.html', {})
+
+
+# system settings
+def system_settings(request):
+    return render(request, 'system_settings.html', {})
+
+
+# admin profile 
+def admin_profile(request):
+    return render(request, 'admin_profile.html', {})
+
+
+# notification 
+def notifications(request):
+    return render(request, 'notification.html', {})
